9e3b80c9af1f361d0ace7474215a1a1945309dfc
guilherme <guicrga@gmail.com>
2021-01-19 13:31:43 -0300

    primeiro crud com golang

---
+++ CRUD/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Book struct {
+	ID    int    `json: "id"`
+	Title string `json: "title"`
+	Year  int    `json: "year"`
+}
+
+// Books list
+
+var books []Book = []Book{
+	Book{
+		ID:    1,
+		Title: "A câmara secreta",
+		Year:  1997,
+	},
+	Book{
+		ID:    2,
+		Title: "dom casmurro",
+		Year:  1899,
+	},
+}
+
+func serverConfig() {
+	httpAddr := ":5000"
+
+	//routes
+	routes()
+
+	fmt.Println("server is running in the port", httpAddr)
+	err := http.ListenAndServe(httpAddr, nil)
+	if err != nil {
+		log.Fatal("err")
+	}
+
+}
+
+func routes() {
+	http.HandleFunc("/", WelcomeHandler)
+	http.HandleFunc("/Books", httpControler)
+	//http.HandleFunc("Books", NewBookHandler)
+}
+
+func main() {
+	serverConfig()
+}
+
+// Handlers
+
+func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
+}
+
+func ListBooksHandler(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(books)
+}
+
+func NewBookHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("err")
+	}
+	var NewBook Book
+	json.Unmarshal(body, &NewBook)
+	fmt.Println(NewBook)
+	books = append(books, NewBook)
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(NewBook)
+
+}
+
+func httpControler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ListBooksHandler(w, r)
+
+	} else if r.Method == "POST" {
+		NewBookHandler(w, r)
+	}
+
+}
